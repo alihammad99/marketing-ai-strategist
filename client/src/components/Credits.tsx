9cb69b7684163f267f52b2134149a68a4b4949cb
Ali Hammad <aliressan.ar@gmail.com>
2025-11-23 00:08:11 +0300

    credits

--- client/src/components/Credits.tsx
+++ client/src/components/Credits.tsx
@@ -2,16 +2,17 @@ import { useEffect, useState } from "react";
 import { MarketingPlan } from "../types";
 
 const Credits = ({ planHistory }: { planHistory: MarketingPlan[] }) => {
-  const [credits, setCredits] = useState(200);
+  const [credits, setCredits] = useState(0);
 
   useEffect(() => {
     const empty = planHistory?.length > 3;
     if (empty) setCredits(0);
     else {
-      const result = 200 / planHistory?.length;
+      const result = 200 - planHistory?.length * 50;
       setCredits(result);
     }
   }, [planHistory]);
+
   return (
     <p className="font-medium text-base text-slate-500 px-0.5">
       Credits: {credits} 💸
